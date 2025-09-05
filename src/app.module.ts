97a5505dd0055edf6eda74dc0183a4ee9d3d16f8
shahadat <shahadathossan937@gmail.com>
2025-09-05 16:43:02 +0600

    "database connection with Async Approach"

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,31 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
 import { TweetModule } from './tweet/tweet.module';
 import { UsersModule } from './users/users.module';
 import { UsersService } from './users/users.service';
-import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [TweetModule, UsersModule, AuthModule],
+  imports: [
+    TweetModule,
+    UsersModule,
+    AuthModule,
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: 'postgres',
+        host: 'localhost',
+        port: 5432,
+        username: 'postgres',
+        password: 'admin123',
+        database: 'nestjs',
+        entities: [],
+        synchronize: true,
+      }),
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService,UsersService],
+  providers: [AppService, UsersService],
 })
 export class AppModule {}
